f286b76c9a270e75f98c82e2be46aec9178e6615
CoWeDeveloper <owaisilyas22@gmail.com>
2024-03-13 12:39:44 +0500

    first commit

---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Transformation = () =>{
+  return (
+    <div>
+      Transforamtion
+    </div>
+  )
+}
+
+export default Transformation
+
+// https:localhost:3000/transformation/[id]
